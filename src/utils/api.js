30eda0eda765af27fecb861e70864b07a7364050
ffm-id <ferialfahri@gmail.com>
2022-06-14 15:19:40 +0100

    Adding parametric endpoint to api request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const ncNewsApi = axios.create({
-    baseURL: "https://nc-news-feri.herokuapp.com/api",
-})
+  baseURL: "https://nc-news-feri.herokuapp.com/api",
+});
 
-export const getArticles = () => {
-    return ncNewsApi.get("/articles").then(({data}) => {
-        return data;
-    })
-}
+export const getArticles = (topic) => {
+  return ncNewsApi.get("/articles", { params: { topic } }).then(({ data }) => {
+    return data;
+  });
+};
