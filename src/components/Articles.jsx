dad0e1955a2141b5fd968ee523c19503d30789e9
ffm-id <ferialfahri@gmail.com>
2022-06-15 11:10:53 +0100

    Linking articles with article

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import { getArticles } from "../utils/api";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-function Articles() {
+function Articles({ topicFromArticle }) {
   const [currArticles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isFound, setIsFound] = useState(true);
 
-  const { topic } = useParams();
+  let { topic } = useParams();
+
+  if (topicFromArticle) {
+    topic = topicFromArticle;
+  }
 
   useEffect(() => {
     getArticles(topic).then((articlesFromApi) => {
@@ -19,11 +23,16 @@ function Articles() {
     });
   }, [topic]);
 
-  if (isLoading) return <p>... loading</p>;
-  if (!isFound)
+  if (isLoading) {
+    return <p>... loading</p>;
+  }
+
+  if (!isFound) {
     return (
       <p className="error">Oppss... some error occured with {topic} topic</p>
     );
+  }
+
   return (
     <>
       <div className="articles">
@@ -32,7 +41,11 @@ function Articles() {
             return (
               <li key={article.article_id} className="article-card">
                 <article>
-                  <h2>{article.title}</h2>
+                  <h2>
+                    <Link to={`/articles/${article.article_id}`} target="_top">
+                      {article.title}
+                    </Link>
+                  </h2>
                   <div className="article-info">
                     <p>Created: {article.created_at.slice(0, 10)}</p>
                     <p>Topic: {article.topic}</p>
